6144f9c57515303819bbf7f844d52f1d1bcfec36
Shubham Raorane <60027310+ShubhamRaorane@users.noreply.github.com>
2020-06-05 23:41:30 +0530

    added new print line

--- intro.py
+++ intro.py
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
-print('this introduction is deprecated.')
-=======
 print('this introduction is not needed.')
->>>>>>> cd57fb9a47fe4fdbee89e1db3ce540d8b19b8d81
+print('but this introduction shows where we come from')
